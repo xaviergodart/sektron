8dc80d533fbc47af46b6baf126af17d862031fdd
Xavier Godart <xavier.godart64@gmail.com>
2023-01-02 00:16:25 +0100

    update status bar

--- ui/status.go
+++ ui/status.go
@@ -5,7 +5,7 @@ import "github.com/charmbracelet/lipgloss"
 var (
 	statusNugget = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("#FFFDF5")).
-			Padding(0, 1)
+			Padding(1, 1)
 
 	statusBarStyle = lipgloss.NewStyle().
 			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
@@ -15,7 +15,7 @@ var (
 			Inherit(statusBarStyle).
 			Foreground(lipgloss.Color("#FFFDF5")).
 			Background(lipgloss.Color("#FF5F87")).
-			Padding(0, 1).
+			Padding(1, 1).
 			MarginRight(1)
 
 	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
@@ -29,10 +29,11 @@ func (m mainModel) renderStatus() string {
 	statusKey := statusStyle.Render("REC")
 	statusTrack := trackStatusStyle.Render("TRACK 1")
 	statusVal := statusText.Copy().
-		Width(m.width - w(statusKey) - w(statusTrack)).
+		Padding(1, m.width-w(statusKey)-w(statusTrack), 1, 0).
+		Align(lipgloss.Center).
 		Render("Playing")
 
-	return lipgloss.JoinHorizontal(lipgloss.Top,
+	return lipgloss.JoinHorizontal(lipgloss.Center,
 		statusTrack,
 		statusKey,
 		statusVal,
